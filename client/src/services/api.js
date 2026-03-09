85037ef65c5b1825768716f9be73441eb7b0f61f
tanzimbn <tanzimbinnasir@gmail.com>
2026-03-09 22:45:58 +0600

    update in package.json

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.API_BASE_URL || 'http://localhost:3000/api/v1',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1',
     withCredentials: true
 });
 
